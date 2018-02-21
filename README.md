4abfb2a31e4927efe67e3bfabada950f981f22a4
Philipp Koota <thefealoce@gmail.com>
2018-02-21 11:04:49 +0100

    locale storageaccess workaround

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Uni_javascript
+
+Házi feladatok beadására használt repo...
+
+index.html hosted in github pages.
+
+## lesson_1
+
+console applcation that controls todo list.
---
+++ index.js
@@ -0,0 +1,71 @@
+let todos = [];
+
+const list = document.getElementById("todo_container");
+
+
+function addTodo(todo){
+    todo = todo || {};
+    // console.log(todo);
+    if(typeof todo.label !== "string"){
+        throw new Error("Todo should have a label");
+    }
+    if(typeof todo.isDone !== "boolean"){
+        throw new Error("Todo should have a boolean state (isDome)");
+    }
+    todos.push(todo);
+    saveTodos();
+}
+
+function saveTodos(){
+    localStorage.setItem("saved_todos",JSON.stringify(todos));
+}
+
+function loadTodos(){
+    todos = JSON.parse(localStorage.getItem("saved_todos")) || [];
+}
+
+function printTodos(){
+    todos.forEach((todo)=>{
+        console.log(todo.label, todo.isDone ? "✓":"Work in progress");
+    });
+}
+
+function renderTodos(){
+
+    todos.forEach((todo)=>{
+
+        const todoElem = document.createElement("div");
+        todoElem.appendChild(document.createTextNode(todo.label));
+
+        if(todo.isDone){
+            todoElem.style.textDecoration = "line-through";
+        }
+
+        todoElem.addEventListener("click", ()=>{
+            todo.isDone = !todo.isDone;
+
+            if(todo.isDone){
+                todoElem.style.textDecoration = "line-through";
+            }
+            else{
+                todoElem.style.textDecoration = "";
+            }
+        });
+        todo
+        list.appendChild(todoElem);
+    });
+}
+
+addTodo({
+    label:"learn JavaScript",
+    isDone: true
+});
+
+addTodo({
+    label:"learn Japanese",
+    isDone: false
+});
+
+loadTodos();
+printTodos()
+renderTodos();
